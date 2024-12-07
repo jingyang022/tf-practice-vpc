e05ac7c159266babcca94d8f349fb993f1e847da
jingyang022 <jingyang022@yahoo.com.sg>
2024-12-07 20:14:17 +0800

    updated outputs.tf file

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,15 @@ output "availability_zones" {
   value = data.aws_availability_zones.available.names
 }
 
-output "nat_gateway_ids" {
-    value = module.vpc.natgw_ids
+output "public_subnets" {
+    value = module.vpc.public_subnets
 }
 
 output "private_subnets" {
     value = module.vpc.private_subnets
 }
+
+/* output "nat_gateway_ids" {
+    value = module.vpc.natgw_ids
+} */
+
