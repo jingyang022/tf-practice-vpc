2dccdd5e4746ae67a0f2cb31fb1638447aef942c
jingyang022 <jingyang022@yahoo.com.sg>
2025-02-26 20:19:21 +0800

    updated main.tf

--- main.tf
+++ main.tf
@@ -8,11 +8,15 @@ module "vpc" {
     name = "yap-vpc"
     cidr = "10.0.0.0/16"
     azs = data.aws_availability_zones.available.names
+
     public_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
     private_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+    map_public_ip_on_launch = true
     enable_nat_gateway = false
     single_nat_gateway = true
     enable_dns_hostnames = true
+
     tags = {
         Terraform = "true"
         }
